0c10644d333f7f89f5f2b8c484ae4c3c2465ce85
Andrew Michaud <andrewjmichaud@gmail.com>
2014-07-09 16:03:17 -0700

    Really made symlinks real

---
+++ .config/xmonad/pascal.xmonad.hs
@@ -0,0 +1,236 @@
+-- Andrew Michaud's XMonad config.
+-- Built from bits and pieces of other configs by other people.
+
+import XMonad
+
+import XMonad.Hooks.DynamicLog
+import XMonad.Hooks.ManageDocks
+import XMonad.Hooks.ManageHelpers
+import XMonad.Hooks.UrgencyHook
+
+import XMonad.Layout.NoBorders
+import XMonad.Layout.LayoutHints
+
+import XMonad.Actions.SpawnOn
+
+import XMonad.Util.EZConfig(additionalKeys)
+import XMonad.Util.Run(spawnPipe)
+
+import Graphics.X11.ExtraTypes.XF86
+import Graphics.X11.Xlib
+
+import XMonad.Hooks.FadeInactive
+
+import System.IO
+
+-- Environment variables
+import System.Environment(getEnv)
+
+-- For extra workspace nonsense
+import qualified XMonad.StackSet as W
+
+-- For dealing with Java.
+import XMonad.Hooks.SetWMName
+
+-----------------------------------------------------------------------------------------
+-----------------------------VARIABLES AND STUFF-----------------------------------------
+-----------------------------------------------------------------------------------------
+
+-- Woo XDG base spec.
+xdg_config_home :: String
+xdg_config_home <- getenv("XDG_CONFIG_HOME")
+
+xmonad_home :: String
+xmonad_home = xdg_config_home ++ "/xmonad"
+
+-- Use XFCE4 terminal without menubar
+myTerminal :: String
+myTerminal = "xfce4-terminal --hide-menubar"
+
+terminalCommand :: String -> String
+terminalCommand cmd = myTerminal ++ " -x " ++ cmd
+
+-- ModMask = windowsKey
+myModMask = mod4Mask
+
+--Borders
+myFocusedBorderColor :: String
+myFocusedBorderColor = "turquoise"
+
+myNormalBorderColor :: String
+myNormalBorderColor = "#333333"
+
+myBorderWidth :: Dimension
+myBorderWidth = 2
+
+-- Workspace titles
+myWorkspaces :: [String]
+myWorkspaces = ["` dev",  "1 term", "2 web",  "3 chat", "4 ssh",
+                "5 game", "6 vid",  "7 work", "8 work", "9 work",
+                "0 etc",  "- perf", "= music"]
+
+-- Workspace keys.
+myKeys = [xK_grave] ++ [xK_1 .. xK_9] ++ [xK_0, xK_minus, xK_equal]
+
+-----------------------------------------------------------------------------------------
+-----------------------------STATUS BAR STUFF--------------------------------------------
+-----------------------------------------------------------------------------------------
+
+myTrayer :: String
+myTrayer = "trayer --edge top --align right " ++
+           "--widthtype pixel --width 100 " ++
+           "--expand true --SetDockType true --SetPartialStrut true " ++
+           "--transparent false --expand true " ++
+           "--heighttype pixel --height 16 --padding 2"
+
+-- Bitmaps used to represent current layout.
+myBitmapsPath :: String
+myBitmapsPath = xmonad_home ++ "/dzen/bitmaps/"
+
+-- Dzen handles window info and the right is conky being cool.
+myDzenStatus :: String
+myDzenStatus = "dzen2 -x '0' -w '700' -ta 'l'" ++ myDzenStyle
+
+myDzenConky :: String
+myDzenConky  = "conky -c ~/.conky_dzen | dzen2 -x '700' -w '800' -ta 'r'" ++ myDzenStyle
+
+myDzenStyle :: String
+myDzenStyle = myDzenFont ++ "-h '16' -y '0' -fg '#ffffff' -bg " ++ bgDzen
+
+bgDzen :: String
+bgDzen = "'#383a3b'"
+
+myDzenFont :: String
+-- fndry-fmly-wght-slant-sWdth-adstyl-pxlsz-ptsz-resx-resy-spc-avgWidth-rgstry-encdng
+myDzenFont = " -fn '-*-verdana-medium-r-normal-*-*-100-*-*-p-*-iso10646-1' "
+
+bgXmonad :: String
+bgXmonad = "#383a3b"
+
+-- Pretty printing.
+myDzenPP :: PP
+myDzenPP  = dzenPP
+    { ppCurrent = dzenColor bgXmonad "#gray" . pad
+    , ppHidden  = dzenColor "gray" "" . pad . take 1
+    , ppUrgent  = dzenColor "#ff0000" "" . pad
+    , ppSep     = "|"
+    , ppTitle   = shorten 150 . dzenColor "white" "" . pad
+    , ppLayout  = \x -> case x of
+                      "Tall"        -> wrapBitmap "rob/tall.xbm"
+                      "Mirror Tall" -> wrapBitmap "rob/mtall.xbm"
+                      "Full"        -> wrapBitmap "rob/full.xbm"
+    }
+    where wrapBitmap bitmap = "^p(8)^i(" ++ myBitmapsPath ++ bitmap ++ ")^p(8)"
+
+-----------------------------------------------------------------------------------------
+----------------------------------CUSTOM HOOKS-------------------------------------------
+-----------------------------------------------------------------------------------------
+
+-- Custom manage hook.
+myManageHook :: ManageHook
+myManageHook = composeAll
+    [ className =? "Xfce4-notifyd"  --> doIgnore
+
+    , title     =? "__NCMPCPP"      --> doCenterFloat
+    , title     =? "__ALSAMIXER"    --> doCenterFloat
+    , title     =? "__XMONADHS"     --> doCenterFloat
+    , title     =? "__HTOP"         --> doShift "-:perf"
+
+    , className =? "Gimp"           --> doFloat
+
+    , className =? "mpv"            --> doFullFloat
+
+    -- Chat windows go to workspace 3
+    , className =? "Pidgin"         --> doShift "3:chat"
+    , title     =? "SKype"          --> doShift "3:chat"
+    ]
+
+-- Custom log hook.
+-- Forward window information to dzen bar, formatted.
+myLogHook :: Handle -> X ()
+myLogHook h = dynamicLogWithPP myDzenPP { ppOutput = hPutStrLn h }
+
+-- END CUSTOM HOOKS --
+
+-- Main.
+main :: IO ()
+main = do
+
+    -- Status bar programs.
+    status <- spawnPipe myDzenStatus
+    conky  <- spawnPipe myDzenConky
+    trayer <- spawnPipe myTrayer
+
+    xmonad $ withUrgencyHook NoUrgencyHook defaultConfig
+
+        -- Hooks.
+        { manageHook = manageDocks <+> myManageHook <+> manageHook defaultConfig
+        , layoutHook = avoidStruts $ smartBorders $ layoutHook defaultConfig
+        , logHook = myLogHook status
+
+        -- Handles Java.
+        , startupHook = setWMName "LG3D"
+
+        -- Stuff.
+        , modMask = myModMask
+        , workspaces = myWorkspaces
+        , terminal = myTerminal
+
+        -- Mice are for squares.
+        , focusFollowsMouse = False
+
+        -- Border jazz
+        , focusedBorderColor = myFocusedBorderColor
+        , normalBorderColor  = myNormalBorderColor
+        , borderWidth        = myBorderWidth
+
+        } `additionalKeys`
+
+        ([ ((mod4Mask .|. shiftMask, xK_l),      spawn "xscreensaver-command -lock")
+
+        -- Volume.
+        , ((0, xF86XK_AudioLowerVolume),         spawn "amixer set Master 2-")
+        , ((0, xF86XK_AudioRaiseVolume),         spawn "amixer set Master 2+")
+        , ((0, xF86XK_AudioMute),                spawn "amixer set Master toggle")
+
+        -- Music controls. Should be on relevant xF86 keys, but my keyboard doesn't
+        -- have those.
+        , ((shiftMask, xF86XK_AudioMute),        spawn "mpc toggle")
+        , ((shiftMask, xF86XK_AudioRaiseVolume), spawn "mpc next")
+        , ((shiftMask, xF86XK_AudioLowerVolume), spawn "mpc prev")
+
+        -- Backlight.
+        , ((0, xF86XK_MonBrightnessDown),        spawn "xbacklight -dec 7")
+        , ((0, xF86XK_MonBrightnessUp),          spawn "xbacklight -inc 7")
+
+        -- Screenshots.
+        , ((0, xK_Print),                        spawn "scrot")
+        , ((controlMask, xK_Print),              spawn "sleep 0.2; scrot -s")
+
+        -- Modified to kill taskbar programs.
+        , ((myModMask, xK_q),                    spawn $ "killall dzen2 conky trayer;" ++
+                                                       " xmonad --recompile;" ++
+                                                       " xmonad --restart")
+        -- Alsamixer, ncmpcpp, xmonad.hs quick spawn bindings.
+        , ((myModMask, xK_a),                    spawn $ myTerminal ++
+                                                       " --title=__ALSAMIXER -x alsamixer")
+        , ((myModMask, xK_o),                    spawn $ myTerminal ++
+                                                       " --title=__NCMPCPP -x ncmpcpp")
+        , ((myModMask .|. controlMask, xK_x),    spawn $ myTerminal ++
+                                                       " --title=__XMONADHS -x" ++
+                                                       " vim ~/.xmonad/xmonad.hs")
+
+        , ((myModMask, xK_e),                    spawn "~/bin/toggleOneko")
+        , ((myModMask, xK_w),                    spawn  "x-www-browser")
+        -- various utility scripts
+        , ((myModMask .|. shiftMask, xK_s),      spawn "~/bin/setWallpaper")
+
+        ] ++
+
+        -- Keys for extra workspaces
+        [( (myModMask .|. shiftMask, k), windows $ W.shift i) |
+         (i, k) <- zip myWorkspaces myKeys] ++
+
+        [( (myModMask, k),          windows $ W.greedyView i) |
+         (i, k) <- zip myWorkspaces myKeys])
+
